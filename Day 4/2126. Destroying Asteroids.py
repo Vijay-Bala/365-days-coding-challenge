bc5d9fb7f8384f0905669c0db2d5fb26f43cc0ed
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-10-25 21:23:15 +0530

    Delete Day 4 directory

--- Day 4/2126. Destroying Asteroids.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def asteroidsDestroyed(self, y: int, x: List[int]) -> bool:
-        for i in sorted(x):
-            if i>y: return False
-            y+=i
-        return True
-        
