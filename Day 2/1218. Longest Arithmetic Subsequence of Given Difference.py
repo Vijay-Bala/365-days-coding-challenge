ffa17a518efd8b3c0b66ee026c2b4646d64a1d32
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-10-25 21:22:54 +0530

    Delete Day 2 directory

--- Day 2/1218. Longest Arithmetic Subsequence of Given Difference.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestSubsequence(self, x: List[int], y: int) -> int:
-        z={}
-        c=1
-        for i in x:
-            if i-y in z: z[i]=z[i-y]+1
-            else: z[i]=1
-            c=max(c,z[i])
-        return c
