fb8a4797698ced57ba4c4d593215f2a3e1ad8b09
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-10-25 21:24:29 +0530

    Delete Day 6 directory

--- Day 6/77. Combinations.py
+++
@@ -1,4 +0,0 @@
-from itertools import combinations as c
-class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        return c(range(1,n+1),k)
--- Day 6/991. Broken Calculator.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def brokenCalc(self, x: int, y: int) -> int:
-        c=0
-        while y>x:
-            if y%2==0: y//=2;c+=1
-            else: y+=1;c+=1
-        return c+x-y
