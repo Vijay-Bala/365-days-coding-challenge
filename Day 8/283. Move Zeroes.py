5c8af4b8eb552180fd5e8f04433175c9547816ca
Vijay Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-10-25 21:24:49 +0530

    Delete Day 8 directory

--- Day 8/283. Move Zeroes.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def moveZeroes(self, x: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        for i in range(x.count(0)): x.remove(0);x.append(0)
